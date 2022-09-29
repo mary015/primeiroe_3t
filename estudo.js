d3a8293b3a3dd57c806854009744e1a086d9680a
Mariana Macedo n42 <108184609+mary015@users.noreply.github.com>
2022-09-29 09:07:30 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,17 @@
+let tabuada = 11; 
+
+function escreva(){
+    document.write( " <h1>Tabuada do" + tabuada + "<br> </h1>");
+    document.write( tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    document.write( tabuada + "x 2 ="+ (tabuada*2)+"<br>");
+    document.write( tabuada + "x 3 ="+ (tabuada*6)+"<br>");
+    document.write( tabuada + "x 4 ="+ (tabuada*8)+"<br>");
+    document.write( tabuada + "x 5 ="+ (tabuada*10)+"<br>");
+    document.write( tabuada + "x 6 ="+ (tabuada*12)+"<br>");
+    document.write( tabuada + "x 7 ="+ (tabuada*14)+"<br>");
+    document.write( tabuada + "x 8 ="+ (tabuada*16)+"<br>");
+    document.write( tabuada + "x 9 ="+ (tabuada*18)+"<br>");
+    document.write( tabuada + "x 10 ="+ (tabuada*20)+"<br>");
+}
+
 
